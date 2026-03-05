8365856b57081ef665e5d9907fb13a3ffa12c834
dilip <dilip@gmail.com>
2026-03-05 09:27:19 +0530

    undefind type fixed

--- client/redux/utils/authStorages.ts
+++ client/redux/utils/authStorages.ts
@@ -1,19 +1,19 @@
 import { SIGNIN_RESPONSE } from "@/types/Auth"
 
 export const setStorage = (data: SIGNIN_RESPONSE) => {
-    if (typeof window === undefined) {
+    if (typeof window === "undefined") {
         return
     }
     localStorage.setItem("ADMIN", JSON.stringify(data.result))
 }
 export const getStorage = () => {
-    if (typeof window === undefined) {
+    if (typeof window === "undefined") {
         return
     }
     return JSON.parse(localStorage.getItem("ADMIN") as string)
 }
 export const removeStorage = () => {
-    if (typeof window === undefined) {
+    if (typeof window === "undefined") {
         return
     }
     localStorage.removeItem("ADMIN")
